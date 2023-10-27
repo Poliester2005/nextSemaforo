543a9a6759fce8d56b82a7c5dcc990724b320519
Eduardo Pielich <107966984+Poliester2005@users.noreply.github.com>
2023-10-27 12:01:29 -0300

    sim

--- src/Estado2.jsx
+++ src/Estado2.jsx
@@ -9,54 +9,54 @@ export default function Estado2() {
                     <div></div>
                     <div className='SemSec Meio'>
                         <label>
-                            <input type='radio' Name='SemSec' className='SemVerm' checked='checked'></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSec' className='SemVerm' checked='checked'></input>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
-                            <input type='radio' Name='SemSec' className='SemAma'></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSec' className='SemAma'></input>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
-                            <input type='radio' Name='SemSec' className='SemVerd'></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSec' className='SemVerd'></input>
+                            <span className="checkmark"></span>
                         </label>
                     </div>
                     <div></div>
                     <div className='SemPrincPed Meio'>
                         <label>
                             <input type='radio' name='SemPrincPed' className='SemVerm' checked='checked'></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
                             <input type='radio' name='SemPrincPed' className='SemVerd'></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                     </div>
                     <div className='Meio'></div>
                     <div className='SemPrinc Meio'>
                         <label>
                             <input type='radio' name='SemPrinc' className='SemVerm' ></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
                             <input type='radio' name='SemPrinc' className='SemAma' checked='checked'></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
                             <input type='radio' name='SemPrinc' className='SemVerd' ></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                     </div>
                     <div></div>
                     <div className='SemSecPed Meio'>
                         
                         <label>
-                            <input type='radio' Name='SemSecPed' className='SemVerm' ></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSecPed' className='SemVerm' ></input>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
-                            <input type='radio' Name='SemSecPed' className='SemVerd' checked='checked'></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSecPed' className='SemVerd' checked='checked'></input>
+                            <span className="checkmark"></span>
                         </label>
                         <div className='FeixasVContainer'>
                             <div className='FaixaV'></div>
--- src/Estado3.jsx
+++ src/Estado3.jsx
@@ -9,54 +9,54 @@ export default function Estado3() {
                     <div></div>
                     <div className='SemSec Meio'>
                         <label>
-                            <input type='radio' Name='SemSec' className='SemVerm' ></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSec' className='SemVerm' ></input>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
-                            <input type='radio' Name='SemSec' className='SemAma'></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSec' className='SemAma'></input>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
-                            <input type='radio' Name='SemSec' className='SemVerd' checked='checked'></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSec' className='SemVerd' checked='checked'></input>
+                            <span className="checkmark"></span>
                         </label>
                     </div>
                     <div></div>
                     <div className='SemPrincPed Meio'>
                         <label>
                             <input type='radio' name='SemPrincPed' className='SemVerm' ></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
                             <input type='radio' name='SemPrincPed' className='SemVerd' checked='checked'></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                     </div>
                     <div className='Meio'></div>
                     <div className='SemPrinc Meio'>
                         <label>
                             <input type='radio' name='SemPrinc' className='SemVerm' checked='checked'></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
                             <input type='radio' name='SemPrinc' className='SemAma'></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
                             <input type='radio' name='SemPrinc' className='SemVerd' ></input>
-                            <span class="checkmark"></span>
+                            <span className="checkmark"></span>
                         </label>
                     </div>
                     <div></div>
                     <div className='SemSecPed Meio'>
                         
                         <label>
-                            <input type='radio' Name='SemSecPed' className='SemVerm' checked='checked'></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSecPed' className='SemVerm' checked='checked'></input>
+                            <span className="checkmark"></span>
                         </label>
                         <label>
-                            <input type='radio' Name='SemSecPed' className='SemVerd' ></input>
-                            <span class="checkmark"></span>
+                            <input type='radio' name='SemSecPed' className='SemVerd' ></input>
+                            <span className="checkmark"></span>
                         </label>
                         <div className='FeixasVContainer'>
                             <div className='FaixaV'></div>
