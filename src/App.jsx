21c80aa95b050315ee485a1659aeac3c2885b36e
Eduardo Pielich <107966984+Poliester2005@users.noreply.github.com>
2023-10-27 12:01:11 -0300

    Sim

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 import './App.css'
-import * as mqtt from 'mqtt/dist/mqtt'
 import Estado1 from './Estado1'
 import Estado2 from './Estado2'
 import Estado3 from './Estado3'
+import XMLData from 'https://drive.google.com/open?id=18zLpr4NK1srlyNvS3B4-q-pg0nFd325e&authuser=0';
 
+var rawFile = new XMLHttpRequest();
+rawFile.open("GET", XMLData, false);
+rawFile.onreadystatechange = () => {
+    if (rawFile.readyState === 4) {
+        if (rawFile.status === 200 || rawFile.status == 0) {
+            var xmlasstring = rawFile.responseText;
+            console.log('Your xml file as string', xmlasstring)
+        }
+    }
 
-var options = [{
-  host: 'localhost',
-  port: '1883'
-}]
-
-var client = mqtt.connect("mqtt://46.17.108.113", options);
-
-var topics = "/TEF/lamp114/attrs/S1"
-
-client.subscribe(topics);
-console.log("Client subscribed ");
-
-var estado = 1
 
 function App() {
 
   return (
     <>
-      <Estado3></Estado3>
+      <Estado1></Estado1>
     </>
   )
 }
